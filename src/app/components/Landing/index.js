38d96436155962e9e9256173f41c3306013e6ab6
kpecel <pecelkatuwal@gmail.com>
2022-09-30 22:11:10 +0100

    latest changes

--- src/app/components/Landing/index.js
+++ src/app/components/Landing/index.js
@@ -1,13 +1,25 @@
 import React, { useEffect, useState } from "react";
 import UserDetail from "../user-detail";
+import UserContext from "../../context/userContext";
+
+import { decodeToken } from "../../util/decoder";
+import { get } from "../../util/storageUtil";
 
 const LandingPage = () => {
-  const [name, setName] = useState("peshal");
+  const [data, setData] = useState({});
 
+  const updateDetails = () => {
+    let user = decodeToken(get("local", "loggedInUser"));
+    setData(user);
+  };
+
+  useEffect(() => {
+    updateDetails();
+  });
   return (
-    <div>
+    <UserContext.Provider value={data}>
       <UserDetail />
-    </div>
+    </UserContext.Provider>
   );
 };
 
--- src/app/components/my-details/index.js
+++ src/app/components/my-details/index.js
@@ -1,15 +1,15 @@
-import React from "react";
-
+import React, { useContext } from "react";
+import LoginContext from "../../context/userContext";
 const MyDetails = () => {
+  const data = useContext(LoginContext);
   return (
-    <>
-      <div>
-        <p>Name: Peshal</p>
-        <br />
-        <span>Age: 26</span>
-        <span>Role: Admin</span>
-      </div>
-    </>
+    <div>
+      <span>
+        Name: {data.firstName} {data.lastName}
+      </span>
+      <span>Age: 26</span>
+      <span>Role: Admin</span>
+    </div>
   );
 };
 export default MyDetails;
