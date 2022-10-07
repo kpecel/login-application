f6a2bedfa25a4eee63590c84e7ba0c4b077d8cb4
kpecel <pecelkatuwal@gmail.com>
2022-10-07 21:01:47 +0100

    added React Profiler API

--- src/app/components/Landing/index.js
+++ src/app/components/Landing/index.js
@@ -5,17 +5,27 @@ import { decodeToken } from "../../util/decoder";
 import { get } from "../../util/storageUtil";
 
 const LandingPage = () => {
+  const [data, setData] = useState({ firstName: "", lastName: "", role: "" });
   const updateDetails = () => {
     let user = decodeToken(get("local", "loggedInUser"));
+    updateStateData(user);
+  };
+
+  const updateStateData = (user) => {
+    setData({
+      firstName: user.firstName,
+      lastName: user.lastName,
+      role: user.role,
+    });
   };
 
   useEffect(() => {
     updateDetails();
-  });
+  }, []);
 
   return (
     <div>
-      <UserDetail />
+      <UserDetail data={data} />
     </div>
   );
 };
--- src/app/components/user-detail/index.js
+++ src/app/components/user-detail/index.js
@@ -1,11 +1,12 @@
 import React from "react";
 import MyDetails from "../my-details";
 
-const UserDetail = () => {
+const UserDetail = (props) => {
+  const { data } = props;
   return (
     <div>
       <p>This is my Details</p>
-      <MyDetails />
+      <MyDetails data={data} />
     </div>
   );
 };
